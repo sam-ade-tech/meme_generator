c6650a0bfc8266932478b723484c4489510e5036
BlessingAdewale <adelekeblessingadewale@gmail.com>
2026-07-13 12:08:46 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header  from "./Header";
+import MainContent from "./MainContent";
+
+const App = () => {
+  return ( 
+    <> 
+       <Header  />
+       <MainContent />
+    
+    </>
+   );
+}
+ 
+export default App;
